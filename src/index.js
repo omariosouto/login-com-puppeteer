6f0169b2e584a1ebfe1584a14dc580afe58fce93
Jece Xavier <jece.pereira@etec.sp.gov.br>
2021-07-12 09:14:41 -0300

    fix: updating init function

--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ const { beatTime } = require('./beatTime');
 const { iHaveJob } = require('./getInfoDay');
 const { notifyMe } = require('./notification');
 
-//State of company
-const ufCompany = 'SP';
+const init = async () => {
+  //State of company
+  const ufCompany = 'SP';
 
-//Get date today
-const today = new Date();
-// const tomorrow = new Date(today.setDate(today.getDate() + 0));
+  //Get date today
+  const today = new Date();
+  // const tomorrow = new Date(today.setDate(today.getDate() + 0));
 
-let message;
+  let message;
 
-if(iHaveJob(today, ufCompany)){
-  console.log(`I have a job!!`)
-  beatTime();
-  message = 'Your point has been hit!! :D' 
-}else{
-   message = `Today not need to beat time!! :D`; 
+  if(iHaveJob(today, ufCompany)){
+    console.log(`I have a job!!`)
+    await beatTime();
+    message = 'Your point has been hit!! :D' 
+  }else{
+    message = `Today not need to beat time!! :D`; 
+  }
+
+  console.log(message);
+  notifyMe(message);
 }
 
-console.log(message);
-notifyMe(message);
-
-
+init();
